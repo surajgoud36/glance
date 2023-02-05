98874482826e9d8a52b490f6de9a80ea72e0a9f0
Suraj Goud Rachamalla <112563526+surajgoud36@users.noreply.github.com>
2023-02-04 21:14:01 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
   const[r2,setR2]=useState();
   const Add=()=>{
     setR1(Number(first)+Number(sec));
-    Axios.get(`http://localhost:3001/add/${first}/and/${sec}`).then((response)=>{
+    Axios.get(`http://54.90.4.35:3001/add/${first}/and/${sec}`).then((response)=>{
       setR2(Number(response.data.addResult));
       console.log(response.data.addResult);
     });
